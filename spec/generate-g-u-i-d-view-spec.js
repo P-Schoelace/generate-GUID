d8f0c408cc56be786caa88cd2b19db41df3a8bf1
Wout van Kampen <wout@yaacomm.com>
2019-11-07 09:18:34 +0100

    Initial commit

---
+++ spec/generate-g-u-i-d-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GenerateGUIDView from '../lib/generate-g-u-i-d-view';
+
+describe('GenerateGUIDView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
