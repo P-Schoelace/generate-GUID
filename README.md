e8e1304323c13f4e7f7d7df7b2a31cae4eefc8a5
Peter Schoenveter <woutvankampen@hotmail.com>
2019-11-07 19:56:26 +0100

    Cleanup
    Removed unused generate-g-u-i-d-view
    Updated License and Readme,

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # generate-g-u-i-d package
 
 Package for generating x amount for GUID keys
+
+Use ctrl + alt + g to trigger
--- spec/generate-g-u-i-d-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import GenerateGUIDView from '../lib/generate-g-u-i-d-view';
-
-describe('GenerateGUIDView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
